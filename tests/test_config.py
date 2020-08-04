9ce552ee0e02d808cdca3f4c48205b86b1caa265
dongkyutoronto <68974415+dongkyutoronto@users.noreply.github.com>
2020-08-04 01:59:05 -0400

    config module unit test broke the other unit tests
    Back up and restore to the original values.

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,6 +10,22 @@ import hqtrivia.config as config
 
 class ConfigTest(unittest.TestCase):
 
+    def setUp(self):
+        # Back them up
+        self.CONFIG_LOG_LEVEL = config.CONFIG_LOG_LEVEL
+        self.CONFIG_PLAYERS_PER_GAME = config.CONFIG_PLAYERS_PER_GAME
+        self.CONFIG_WS_SERVER_PORT = config.CONFIG_WS_SERVER_PORT
+        self.CONFIG_ROUND_DURATION = config.CONFIG_ROUND_DURATION
+        self.CONFIG_QUESTION_GENERATOR_API = config.CONFIG_QUESTION_GENERATOR_API
+
+    def tearDown(self):
+        # Restore them
+        config.CONFIG_LOG_LEVEL = self.CONFIG_LOG_LEVEL
+        config.CONFIG_PLAYERS_PER_GAME = self.CONFIG_PLAYERS_PER_GAME
+        config.CONFIG_WS_SERVER_PORT = self.CONFIG_WS_SERVER_PORT
+        config.CONFIG_ROUND_DURATION = self.CONFIG_ROUND_DURATION
+        config.CONFIG_QUESTION_GENERATOR_API = self.CONFIG_QUESTION_GENERATOR_API
+
     def test_load_config(self):
         """Tests load_config method
         """
